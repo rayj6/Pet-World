b5f44a3b237372c3e938ea8a9d48b2cd4950c01a
rayj6 <nguyentu5526@gmail.com>
2024-12-03 02:35:13 +0700

    first comback

--- App.js
+++ App.js
@@ -10,14 +10,17 @@ import Message from "./mainScreens/Message";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="LandingPage" component={LandingPage} />
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Register" component={Register} />
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="LandingPage"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
